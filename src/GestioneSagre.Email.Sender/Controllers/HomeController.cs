c82e0e20fbe8e5fc8a7382037d72e988d7b2cb57
Angelo Pirola <angelo9871@gmail.com>
2023-02-17 23:36:07 +0100

    Primo commit

---
+++ src/GestioneSagre.Email.Sender/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using GestioneSagre.Email.Sender.BusinessLayer.Command;
+using GestioneSagre.Email.Sender.Controllers.Common;
+using GestioneSagre.Email.Sender.DataAccessLayer;
+using GestioneSagre.Email.Sender.Shared.Enums;
+using GestioneSagre.Email.Sender.Shared.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestioneSagre.Email.Sender.Controllers;
+
+public class HomeController : BaseController
+{
+    private readonly ILogger<HomeController> logger;
+    private readonly EmailSenderDbContext dbContext;
+    private readonly IMediator mediator;
+
+    public HomeController(ILogger<HomeController> logger, EmailSenderDbContext dbContext, IMediator mediator)
+    {
+        this.logger = logger;
+        this.dbContext = dbContext;
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendEmailAsync(EmailRequest request)
+    {
+        try
+        {
+            var result = await mediator.Send(new SendEmailCommand(request));
+
+            var message = await dbContext.EmailMessages
+                .AsNoTracking()
+                .Where(x => x.MessageId == request.MessageId)
+                .FirstOrDefaultAsync();
+
+            if (!result.Succeeded)
+            {
+                var entity1 = await dbContext.EmailMessages.FindAsync(message.Id);
+                var newCount = entity1.SenderCount + 1;
+
+                entity1.ChangeSenderCount(newCount);
+
+                await dbContext.SaveChangesAsync();
+
+                return BadRequest();
+            }
+
+            var entity2 = await dbContext.EmailMessages.FindAsync(message.Id);
+            entity2.ChangeStatus(MailStatus.Sent);
+
+            await dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ src/GestioneSagre.Email.Sender/Startup.cs
@@ -0,0 +1,89 @@
+﻿using GestioneSagre.Email.Sender.BusinessLayer.Handlers;
+using GestioneSagre.Email.Sender.BusinessLayer.Services;
+using GestioneSagre.Email.Sender.DataAccessLayer;
+using GestioneSagre.Email.Sender.Shared.Options;
+using MediatR;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace GestioneSagre.Email.Sender;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("GestioneSagre.Email.Sender", policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(config =>
+        {
+            config.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Gestione Sagre Email Sender",
+                Version = "v1"
+            });
+        });
+
+        services.AddDbContextPool<EmailSenderDbContext>(optionsBuilder =>
+        {
+            var connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+
+            optionsBuilder.UseSqlServer(connectionString, options =>
+            {
+                //Creazione migration: Add-Migration NOME-MIGRATION -Project GestioneSagre.Email.Sender.Migrations
+                //Esecuzione migration: Update-Database
+                options.MigrationsAssembly("GestioneSagre.Email.Sender.Migrations");
+
+                // Abilito il connection resiliency per gestire le connessioni perse
+                // Info su: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
+                options.EnableRetryOnFailure(3);
+            });
+        });
+
+        services.AddSingleton<IEmailSender, MailKitEmailSender>();
+        services.AddSingleton<IEmailClient, MailKitEmailSender>();
+
+        services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
+        services.Configure<SmtpOptions>(Configuration.GetSection("Smtp"));
+
+        services.AddMediatR(typeof(SendEmailHandler).Assembly);
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        //app.UseHttpsRedirection();
+        app.UseCors("GestioneSagre.Email.Sender");
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre Email Sender v1");
+        });
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
